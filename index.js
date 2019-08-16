96214ab516e265fe5500197b29afc586550f2438
Michał Wąsowicz <mwasowicz7@gmail.com>
2019-08-17 00:54:59 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const cookieAcceptedKey = 'cookies-accepted';
+
+function createNumberedColumn() {
+    const numbersColumn = $('#numbers-col');
+
+    const numberOfRows = getNumberOfRows();
+    for (let i = 1; i<numberOfRows; i++) {
+        const number = document.createElement('p');
+        number.innerText = i;
+        numbersColumn.append(number);
+    }
+}
+
+function getNumberOfRows() {
+    return Math.round($('.container').height()/15)
+}
+
+function acceptCookie() {
+    document.cookie = cookieAcceptedKey;
+}
+
+$(document).ready(function () {
+    createNumberedColumn();
+
+    if (!document.cookie.includes(cookieAcceptedKey)) {
+        $('.toast').toast('show')
+    }
+
+    $('.toast').on('hidden.bs.toast', acceptCookie);
+});
+
+$(window).resize(function () {
+    $('#numbers-col').empty();
+    createNumberedColumn()
+});
+
+
